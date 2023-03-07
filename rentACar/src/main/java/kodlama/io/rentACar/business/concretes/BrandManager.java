3c49cedd041be15fee89d4b0f803c4bb063fa3bb
Ramazan Küçükkoç <ramazankucukkoc43@gmail.com>
2023-03-07 14:27:09 +0300

    First commit rentACar.

---
+++ rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -0,0 +1,33 @@
+package kodlama.io.rentACar.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+@Service//Bu sınıf business nesnesidir.
+
+public class BrandManager implements BrandService{
+
+	private BrandRepository brandRepository;
+	@Autowired
+	//IoC işlemelerini üstlenen Anatasyon'dur.Aşagıdaki constructora bakıyor 
+	//BrandRepository interface implements eden sınıfı new'liyor otomatik.
+	
+	public BrandManager(BrandRepository brandRepository) {
+		this.brandRepository = brandRepository;
+	}
+
+	@Override
+	public List<Brand> getAll() {
+//iş kuralları
+		return brandRepository.getAll();
+	}
+
+	
+}
+
