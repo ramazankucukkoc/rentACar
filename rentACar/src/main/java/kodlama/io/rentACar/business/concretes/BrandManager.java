611c99c898a1e16fb07ed6c25d19f8461ab86515
Ramazan Küçükkoç <ramazankucukkoc43@gmail.com>
2023-03-08 15:13:50 +0300

    GetAll and Add transactions added.

--- rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -1,11 +1,14 @@
 package kodlama.io.rentACar.business.concretes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandResponse;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
@@ -23,11 +26,37 @@ public class BrandManager implements BrandService{
 	}
 
 	@Override
-	public List<Brand> getAll() {
-//iş kuralları
-		return brandRepository.findAll();
+	public List<GetAllBrandResponse> getAll() {
+      //iş kuralları
+
+		List<Brand> brands= brandRepository.findAll();
+		List<GetAllBrandResponse> brandResponse=new ArrayList<GetAllBrandResponse>();
+		for(Brand brand :brands) {
+			GetAllBrandResponse responseItem =new GetAllBrandResponse();
+		    responseItem.setId(brand.getId());
+		    responseItem.setName(brand.getName());
+            brandResponse.add(responseItem);
+		}			
+		return brandResponse ;
+	}
+
+	@Override
+	public void add(CreateBrandRequest createBrandRequest) {
+		// TODO Auto-generated method stub
+		Brand brand =new Brand();
+		brand.setName(createBrandRequest.getName());
+		this.brandRepository.save(brand);
+		
 	}
 
 	
 }
+
+
+
+
+
+
+
+
 
--- rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandResponse;
 
 @RestController//Controller oldugunu yanı Restful servisi oldugunu belirtiyoruz.Anotasyondur.
 @RequestMapping("/api/brands")//Url adresimizi belirtiyoruz.
@@ -20,11 +23,12 @@ public class BrandsController {
 		this.brandService = brandService;
 	}
 	@GetMapping("/getAll")
-	public List<Brand>getAll(){
+	public List<GetAllBrandResponse>getAll(){
 		return brandService.getAll();
 	}
-	
-	
-	
-	
+	@PostMapping("/add")
+	public void Add(@RequestBody() CreateBrandRequest createBrandRequest) {
+		
+		this.brandService.add(createBrandRequest);
+	}	
 }
